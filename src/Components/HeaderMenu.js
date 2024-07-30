60209e46e84b9dd2e38d097bda2c90ffee247b1f
Prakhar Verma <prakharverma1022@gmail.com>
2024-07-30 11:46:54 +0530

    apply tailwind css in ResMenu.js

--- src/Components/HeaderMenu.js
+++ src/Components/HeaderMenu.js
@@ -7,16 +7,16 @@ const HeaderMenu = () => {
     const onlineStatus = useOnlineStatus();
 
     return (
-        <ul className="w-1/2 flex flex-row justify-between items-center">
+        <ul className="w-fit flex flex-row justify-between items-center">
             <li className="text-gray-500 px-4 font-semibold flex flex-row-reverse justify-center items-center border-solid border-2 rounded-full" dir="rtl">
-                {onlineStatus ? <span className="p-2 mr-2 border-solid border-s-2 border-gray-200">Online</span> : <span className="p-2 mr-2 border-solid border-s-2 border-red-400">Offline</span>}
+                {onlineStatus ? <span className="p-2 mr-2 border-solid border-s-2 border-gray-200">Online</span> : <span className="p-2 mr-2 border-solid border-s-2 border-gray-200">Offline</span>}
                 {onlineStatus ? <FaWifi color="green" /> : <FaTimesCircle color="red" />}
             </li>
-            <li className="text-gray-700 hover:text-orange-500 font-semibold"><Link to="/about">About</Link></li>
-            <li className="text-gray-700 hover:text-orange-500 font-semibold"><Link to="/contact">Contact</Link></li>
-            <li className="text-gray-700 hover:text-orange-500 font-semibold"><Link to="/grocery">Grocery</Link></li>
-            <li className="text-gray-700 hover:text-orange-500 font-semibold"><Link to="/signIn">Sign In</Link></li>
-            <li className="text-gray-700 hover:text-orange-500 font-semibold"><Link to="/cart">Cart</Link></li>
+            <li className="pl-16 xl:pl-16 lg:pl-12 md:pl-8 text-gray-700 hover:text-amber-600 font-semibold transition-all ease-in-out delay-50"><Link to="/about">About</Link></li>
+            <li className="pl-16 xl:pl-16 lg:pl-12 md:pl-8 text-gray-700 hover:text-amber-600 font-semibold transition-all ease-in-out delay-50"><Link to="/contact">Contact</Link></li>
+            <li className="pl-16 xl:pl-16 lg:pl-12 md:pl-8 text-gray-700 hover:text-amber-600 font-semibold transition-all ease-in-out delay-50"><Link to="/grocery">Grocery</Link></li>
+            <li className="pl-16 xl:pl-16 lg:pl-12 md:pl-8 text-gray-700 hover:text-amber-600 font-semibold transition-all ease-in-out delay-50"><Link to="/signIn">Sign In</Link></li>
+            <li className="pl-16 xl:pl-16 lg:pl-12 md:pl-8 text-gray-700 hover:text-amber-600 font-semibold transition-all ease-in-out delay-50"><Link to="/cart">Cart</Link></li>
         </ul>
     );
 };
--- src/Components/ResMenu.js
+++ src/Components/ResMenu.js
@@ -3,8 +3,8 @@ import useResMenu from '../utils/useResMenu';
 import vegImg from '../images/vegetarian.png';
 import nonVegImg from '../images/non-vegetarian.png';
 
-const ResMenu = () => {    
-    const {resInfo} = useResMenu();
+const ResMenu = () => {
+    const { resInfo } = useResMenu();
 
     if (resInfo === null) return <ShimmerFood />;
 
@@ -19,26 +19,29 @@ const ResMenu = () => {
     if (!itemCards) return <div>No items available</div>;
 
     return (
-        <div className="res-menu-outer-ctr">
-            <h1 className="res-menu-header">{name}</h1>
+        <div className="py-24 px-12 xl:px-96 lg:px-64 md:px-48 sm:px-24">
+            <h1 className="mb-24 font-extrabold text-6xl text-center text-gray-800">{name}</h1>
             {itemCards.map((item) => (
-                <div className="dish-outer-ctr" key={item.card.info.id}>
-                    <div className="dish-details-ctr">
-                        <span className="dish-veg-classifier">
-                            {
-                                item.card.info.itemAttribute.vegClassifier === "VEG" ? <img src={vegImg} /> : <img src={nonVegImg} />
-                            }
-                        </span>
-                        <h2 className="dish-name">{item.card.info.name}</h2>
-                        <p className="dish-price">₹{(item.card.info.defaultPrice || item.card.info.price) / 100}</p>
-                        <p className="dish-description">{item.card.info.description}</p>
+                <div className="py-16 border-t-2 border-gray-200 flex justify-between items-center" key={item.card.info.id}>
+                    <div className="w-2/3 mr-16">
+                        {item.card.info.itemAttribute.vegClassifier === "VEG" ? <img className="mb-4 w-8" src={vegImg} /> : <img className="mb-4 w-8" src={nonVegImg} />}
+                        <h2 className="mb-4 text-4xl font-bold">{item.card.info.name}</h2>
+                        <p className="mb-4 text-4xl font-bold text-amber-600">₹{(item.card.info.defaultPrice || item.card.info.price) / 100}</p>
+                        <p className="text-gray-600 text-2xl">{item.card.info.description}</p>
                     </div>
-                    <div className="dish-image-ctr">
-                        <img
-                            className="dish-image"
-                            src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${item.card.info.imageId}`}
-                            alt={item.card.info.name}
-                        />
+                    <div className="w-1/4 h-64 bg-cover relative">
+                        {item.card.info.imageId !== undefined ? (
+                            <img
+                                className="rounded-xl w-full h-full shadow-2xl shadow-gray-400"
+                                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${item.card.info.imageId}`}
+                                alt={item.card.info.name}
+                            />
+                        ) : 
+                            <div class="w-full h-full px-16 py-24 rounded-xl bg-gray-400 text-gray-700 text-4xl text-center font-bold shadow-2xl shadow-gray-400">No preview</div>
+                        }
+                        <button className="px-8 py-2 bg-white absolute -bottom-8 left-1/2 -translate-x-1/2 border-2 border-green-600 rounded-lg">
+                            ADD
+                        </button>
                     </div>
                 </div>
             ))}
