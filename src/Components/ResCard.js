e0634c45a58b844531ec63bae606a48c198d0d64
b4n1y4 <riteshgupta7645@gmail.com>
2024-10-06 15:07:26 +0530

    updated res card and search functionality

--- src/Components/ResCard.js
+++ src/Components/ResCard.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const ResCard = ({
-    cloudinaryImageId,
-    name,
-    cuisines,
-    area,
-    lastMileTravelString,
-    costForTwoString,
-    avgRating,
+  cloudinaryImageId,
+  name,
+  cuisines,
+  area,
+  lastMileTravelString,
+  costForTwoString,
+  avgRating,
 }) => {
-    return (
-        <div className="h-full p-2 justify-self-stretch flex flex-col items-center bg-white shadow-sm shadow-slate-400 hover:shadow-2xl hover:shadow-slate-300 transition-all ease-in-out delay-50 rounded-lg">
-            <div className="h-[18rem] w-full overflow-hidden flex justify-center items-center rounded-t-lg">
-                <img className="w-full" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} alt="Restaurant" />
-            </div>
-            <h3 className="text-3xl text-gray-800 font-semibold px-4 py-8">{name}</h3>
-            <div className="p-2 text-2xl text-gray-600 font-semibold">
-                <h4 className="text-gray-500 px-4">{cuisines.slice(0, 4).join(", ")}</h4>
-                <h4 className="text-gray-500 px-4">{area}</h4>
-                <span className="text-gray-500 px-4">
-                    <h4>
-                        <span className="text-yellow-500 mr-4"><i className="fa-solid fa-star"></i></span>
-                        {avgRating}
-                    </h4>
-                </span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-full justify-self-stretch flex flex-col items-center bg-white shadow-sm shadow-slate-400 hover:shadow-lg hover:shadow-[rgba(0,0,0,0.1)]-300 transition-all ease-in-out rounded-[15px] overflow-hidden group">
+      <div className="h-[18rem] w-full overflow-hidden  flex justify-center items-center rounded-t-lg">
+        <img
+          className="w-full group-hover:scale-105 transition-all ease-in-out"
+          src={
+            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+            cloudinaryImageId
+          }
+          alt="Restaurant"
+        />
+      </div>
+      <h3 className="text-3xl text-gray-800 font-semibold px-4 py-8">{name}</h3>
+      <div className="p-2 text-2xl text-gray-600 font-semibold">
+        <h4 className="text-gray-500 px-4">
+          {cuisines.slice(0, 4).join(", ")}
+        </h4>
+        <h4 className="text-gray-500 px-4">{area}</h4>
+        <span className="text-gray-500 px-4">
+          <h4>
+            <span className="text-yellow-500 mr-4">
+              <i className="fa-solid fa-star"></i>
+            </span>
+            {avgRating}
+          </h4>
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default ResCard
+export default ResCard;
--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -1,72 +1,98 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import ResCard from './ResCard';
-import ResNotFound from './ResNotFound';
-import Shimmer from './Shimmer';
-import useRestaurant from '../utils/useRestaurant';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ResCard from "./ResCard";
+import ResNotFound from "./ResNotFound";
+import Shimmer from "./Shimmer";
+import useRestaurant from "../utils/useRestaurant";
 
 // Shows the restaurants
 const Restaurant = () => {
-    const [searchText, setSearchText] = useState("");
-    const [isResFound, setIsResFound] = useState(true);
+  const [searchText, setSearchText] = useState("");
+  const [isResFound, setIsResFound] = useState(true);
 
-    // This custom hook is responsible for fetching the restraunt data from the api and updating state variables listOfRes and filteredListOfRes
-    const { listOfRes, filteredListOfRes, setFilteredListOfRes } = useRestaurant();
+  // This custom hook is responsible for fetching the restraunt data from the api and updating state variables listOfRes and filteredListOfRes
+  const { listOfRes, filteredListOfRes, setFilteredListOfRes } =
+    useRestaurant();
+  console.log(listOfRes);
+  const [searchResults, setSearchResults] = useState([]);
 
-    const searchcall = () => {
-        const filteredListOfRes = listOfRes.filter((res) => {
-            return res.info.name.toLowerCase().includes(searchText.toLowerCase());
-        })
+  const searchcall = () => {
+    const filteredListOfRes = listOfRes.filter((res) => {
+      return res.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
 
-        setFilteredListOfRes(filteredListOfRes);
-        const isResFound = filteredListOfRes.length !== 0 ? true : false;
-        console.log(filteredListOfRes, isResFound);
-        setIsResFound(isResFound)
-    }
-    
-    // If listOfRes contains nothing, show shimmer UI
-    return listOfRes.length === 0 ?
-        <Shimmer />
-        : (
-            <div className="py-16 text-center  bg-[#f1f1f1]">
-                {/* Filter restraunts based on its name */}
-                <div className="w-[90%] mx-auto flex justify-center items-center">
-                    <input className="max-w-[90%] w-[40rem] py-4 px-[1.6rem] text-[1.4rem] sm:text-[1.8rem] text-gray-600 outline-none bg-white border-2 border-solid border-gray-200 transition-all hover:shadow-xl hover:shadow-gray-200 rounded-xl" type="text" placeholder="Search you favourite restaurant..." value={searchText} onChange={(e) => { 
-                        setSearchText(e.target.value)
-                        searchcall()         
-                    }} />
+    setSearchResults(filteredListOfRes);
+  };
 
-                    <input className="py-4 ml-4 px-[1.6rem] text-[1.4rem] sm:text-[1.8rem] outline-none bg-[#5a5aeb] text-white transition-all hover:shadow-xl hover:shadow-[#64646f4d] rounded-xl cursor-pointer" type="button" value="Search" onClick={() => {
-                        const filteredListOfRes = listOfRes.filter((res) => {
-                            return res.info.name.toLowerCase().includes(searchText.toLowerCase());
-                        })
+  useEffect(() => {
+    searchcall();
+  }, [searchText]);
 
-                        setFilteredListOfRes(filteredListOfRes);
-                        const isResFound = filteredListOfRes.length !== 0 ? true : false;
-                        console.log(filteredListOfRes, isResFound);
-                        setIsResFound(isResFound)
-                    }} />
-                </div>
+  useEffect(() => {
+    setSearchResults(listOfRes);
+  }, []);
 
-                {
-                    // if restaurant found, show that else show an error msg that 'restaurant not found'
-                    isResFound ?
-                        <div className="my-16 flex justify-center items-center">
-                            <div className="w-[90%] max-w-[1366px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-content-center gap-12">
-                                {filteredListOfRes.map((restaurant) => {
-                                    return (
-                                        <Link className="no-underline justify-self-center" key={restaurant.info.id} to={"/restaurants/" + restaurant.info.id}>
-                                            <ResCard {...restaurant.info} />
-                                        </Link>
-                                    )
-                                })}
-                            </div>
-                        </div> :
-                        <ResNotFound />
-                }
+  // If listOfRes contains nothing, show shimmer UI
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="py-16 text-center  bg-[#f1f1f1]">
+      {/* Filter restraunts based on its name */}
+      <div className="w-[90%] mx-auto flex justify-center items-center">
+        <input
+          className="max-w-[90%] w-[40rem] py-4 px-[1.6rem] text-[1.4rem] sm:text-[1.8rem] text-gray-600 outline-none bg-white border-2 border-solid border-gray-200 transition-all hover:shadow-xl hover:shadow-gray-200 rounded-xl"
+          type="text"
+          placeholder="Search you favourite restaurant..."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+
+        {/* <input
+          className="py-4 ml-4 px-[1.6rem] text-[1.4rem] sm:text-[1.8rem] outline-none bg-[#5a5aeb] text-white transition-all hover:shadow-xl hover:shadow-[#64646f4d] rounded-xl cursor-pointer"
+          type="button"
+          value="Search"
+          onClick={() => {
+            const filteredListOfRes = listOfRes.filter((res) => {
+              return res.info.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase());
+            });
+
+            setFilteredListOfRes(filteredListOfRes);
+            const isResFound = filteredListOfRes.length !== 0 ? true : false;
+            console.log(filteredListOfRes, isResFound);
+            setIsResFound(isResFound);
+          }}
+        /> */}
+      </div>
+
+      {
+        // if restaurant found, show that else show an error msg that 'restaurant not found'
+        searchResults.length > 0 ? (
+          <div className="my-16 flex justify-center items-center">
+            <div className="w-[90%] max-w-[1366px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-content-center gap-12">
+              {searchResults.map((restaurant) => {
+                return (
+                  <Link
+                    className="no-underline justify-self-center"
+                    key={restaurant.info.id}
+                    to={"/restaurants/" + restaurant.info.id}
+                  >
+                    <ResCard {...restaurant.info} />
+                  </Link>
+                );
+              })}
             </div>
-        );
-}
+          </div>
+        ) : (
+          <ResNotFound />
+        )
+      }
+    </div>
+  );
+};
 
 export default Restaurant;
